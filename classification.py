517a22d82c0879c40f4740f0d0af2aab6b322ca6
MaudBeeckmans <73498415+MaudBeeckmans@users.noreply.github.com>
2021-03-25 15:11:11 +0100

    Update and rename classification_singleframe.py to classification.py
    Classification of all the frames: 
    * classification is done for each frame separately: model trained only on the same frame as the one it is tested on 
    * included a plot of the models accuracy on the training set as well (10-fold-cross-validation).

--- classification.py
+++ classification.py
@@ -10,6 +10,7 @@ import os
 from sklearn.model_selection import StratifiedShuffleSplit
 import numpy as np
 from matplotlib import pyplot as plt
+import pandas as pd
 
 data_path = os.path.join(os.getcwd(), 'data_processed_concat10to11_AUstatic.rds')
 data = pyreadr.read_r(data_path) # also works for RData
@@ -91,56 +92,84 @@ data_df.insert(2, "Cond_binary", data_df_cond_encoded, True)
 # cond_1hot = cat_encoder.fit_transform(data_df['Condition'].to_numpy().reshape(1, -1))
 # cond_1hot = cond_1hot.toarray()
 
-"""Feature-scaling: might have to do this (see book page 109)
-- important: fit the scalers to training set only"""
-
-"""First step: do classification on 1 frame only: here the frame is put to 20"""
-data_f20 = data_df.loc[data_df["frame"] ==20]
-data_f20 = data_f20.reset_index()
-
-split = StratifiedShuffleSplit(n_splits = 1, test_size = 0.2, random_state = 42)
-for train_index, test_index in split.split(data_f20, data_f20["Condition"]):
-    strat_train_set = data_f20.loc[train_index]
-    strat_test_set = data_f20.loc[test_index]
-"""Just as a check whether it worked correctly: 
-    - train_set: 80 smile, 80 frown
-    - test_set: 20 smile, 20 frown"""
-
-#strat_test_set["Condition"].value_counts() / len(strat_test_set)
-#strat_train_set["Condition"].value_counts() / len(strat_train_set)
-
-"""create the x and y variables for strat_train_set & strat_test_set"""
-AU_col = [col for col in data_f20.columns if ('AU' in col and '_r' in col)] 
-train_x, train_y = strat_train_set[AU_col], strat_train_set['Cond_binary']
-test_x, test_y = strat_test_set[AU_col], strat_test_set['Cond_binary']
-#transform the y-data to integers, as this is often required by ML algorithms
-train_y = train_y.astype(np.uint8)
-test_y = test_y.astype(np.uint8)
-
-"""The actual classfication"""
-from sklearn import svm
-classifier = svm.SVC(kernel = 'linear', C = 1)
-classifier.fit(train_x, train_y)
-classifier.score(test_x, test_y)
-
-"""work with k-fold cross-validation"""
-from sklearn.model_selection import cross_val_score
-cross_scores = cross_val_score(classifier, train_x, train_y, cv=10, scoring="accuracy")
-
+"""Create some functions to use in the loop"""
 def display_scores(scores):
     print("Scores:", scores)
     print("Mean:", scores.mean())
     print("Standard deviation:", scores.std())
+        
+def end_scores(scores): 
+    mean = scores.mean()
+    std = scores.std()
+    return mean, std 
+
+all_mean = []
+all_std = []
+
+for this_frame in np.unique(data_df['frame']):
+    """First step: do classification on 1 frame only: here the frame is put to 20"""
+    data_f = data_df.loc[data_df["frame"] ==this_frame]
+    data_f = data_f.reset_index()
     
-display_scores(cross_scores)
+    split = StratifiedShuffleSplit(n_splits = 1, test_size = 0.2, random_state = 42)
+    for train_index, test_index in split.split(data_f, data_f["Condition"]):
+        strat_train_set = data_f.loc[train_index]
+        strat_test_set = data_f.loc[test_index]
+    """Just as a check whether it worked correctly: 
+        - train_set: 80 smile, 80 frown
+        - test_set: 20 smile, 20 frown"""
+    
+    #strat_test_set["Condition"].value_counts() / len(strat_test_set)
+    #strat_train_set["Condition"].value_counts() / len(strat_train_set)
+    
+    """create the x and y variables for strat_train_set & strat_test_set"""
+    AU_col = [col for col in data_f.columns if ('AU' in col and '_r' in col)] 
+    train_x, train_y = strat_train_set[AU_col], strat_train_set['Cond_binary']
+    test_x, test_y = strat_test_set[AU_col], strat_test_set['Cond_binary']
+    #transform the y-data to integers, as this is often required by ML algorithms
+    train_y = train_y.astype(np.uint8)
+    test_y = test_y.astype(np.uint8)
+    
+    """Feature-scaling: might have to do this (see book page 109)
+    - important: fit the scalers to training set only
+    - is this correctly done? """
+    from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler()
+    scaler.fit(train_x)
+    train_x_standardized = pd.DataFrame.from_records(scaler.transform(train_x))
+    train_x_standardized.columns = train_x.columns
+    test_x_standardized = pd.DataFrame.from_records(scaler.transform(test_x))
+    test_x_standardized.columns = test_x.columns
+    
+    
+    """The actual classfication"""
+    from sklearn import svm
+    classifier = svm.SVC(kernel = 'linear', C = 1)
+    # classifier.fit(train_x, train_y)
+    # print("The score when trained on train-set, tested on test-set")
+    # print(classifier.score(test_x, test_y)
+    
+    
+    """work with k-fold cross-validation"""
+    print("\nNow doing k-fold cross-validation")
+    from sklearn.model_selection import cross_val_score
+    cross_scores = cross_val_score(classifier, train_x_standardized, train_y, cv=10, scoring="accuracy")
+    
+    
+    display_scores(cross_scores)
+    mean, std = end_scores(cross_scores)
+    
+    all_mean.append(mean)
+    all_std.append(std)
 
+fig, axs = plt.subplots(1, 1)
+axs.set_ylim(0, 1.05)
+plt.errorbar(frames, all_mean, yerr = all_std, marker = 'x', color = 'black')
+axs.plot(frames, np.repeat(0.5, len(frames)), color = 'red', label = 'Chance level')
+handles, labels = axs.get_legend_handles_labels()
+fig.legend(handles, labels, loc="upper center")
 
-
-
-
-
-
-
-
-
-
+    
+"""Questions: 
+    - Will we compare different models?
+      --> the k-fold cross-validation: good to see which models perform best?"""
