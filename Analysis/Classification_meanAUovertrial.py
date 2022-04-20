3798a6ef86a835240e50ceaa570ca3c461de8f5b
MaudBeeckmans <maud.beeckmans@outlook.com>
2022-04-20 16:04:30 +0200

    Update Classification_meanAUovertrial.py
    * now classification for frame 0-15 ; 15-45 and 45-60
    * figure is better already
      - idea for even better figure: boxplot & number of * depend on p-value

--- Analysis/Classification_meanAUovertrial.py
+++ Analysis/Classification_meanAUovertrial.py
@@ -44,21 +44,23 @@ from scipy.stats import wilcoxon
 """Select which subset of the data you'd like to use"""
 participants = np.arange(0, 10, 1)
 blocks = np.array([0, 1, 2])
-formats = ['--o', '--x', '--v']
-colors = ['black', 'blue', 'black']
+
 # frames = np.arange(20, 46, 1)
-frames = np.arange(0, 60, 1)
+
+frames_before_stimulus = np.arange(0, 15, 1)
+frames_during_stimulus = np.arange(15, 45, 1)
+frames_after_stimulus = np.arange(45, 60, 1)
+
 
 store_all_means = np.empty([participants.shape[0], blocks.shape[0]])
 store_all_std = np.empty([participants.shape[0], blocks.shape[0]])
 
 AU_col = [col for col in all_data.columns if ('AU' in col and '_r' in col)] 
-
 fixed_cols = ['pp_number', 'block_count', 'Frame_count', 'Trial_number', 'Affect']
 
 
-store_all_means = np.empty([participants.shape[0], blocks.shape[0]])
-store_all_std = np.empty([participants.shape[0], blocks.shape[0]])
+store_all_means = np.empty([participants.shape[0], blocks.shape[0], 3])
+store_all_std = np.empty([participants.shape[0], blocks.shape[0], 3])
 
 
 
@@ -79,44 +81,44 @@ for pp in participants:
     "Creëer nieuwe dataset: voor elke trial slechts 1 value: gemiddelden AU"
     
     
-    
-    # dataset with only the correct frames included
-    relevant_frames_data = test_data[test_data['Frame_count'].isin(frames)]
-    
     #mean AU activation over all frames, but have to do this per trial!!
     for block_select in blocks:
-        relevant_block_data = relevant_frames_data[relevant_frames_data["block_count"] == block_select]
-        
-        unique_values, indices = np.unique(relevant_block_data['Trial_number'], return_index = True)
-        
-        meanAU_blockdata = np.array([np.mean(relevant_block_data[relevant_block_data['Trial_number'] == trial][AU_col], 0) for trial in unique_values])
-        
-        
-        shorter_data = relevant_block_data.iloc[indices, :]
-        shorter_data.loc[:, AU_col] = meanAU_blockdata
-        
-        """Do the classification"""
-        x, y = shorter_data[AU_col], shorter_data['Cond_binary']
-        #transform the y-data to integers, as this is often required by ML algorithms
-        y = y.astype(np.uint8)
+        block_data = test_data[test_data["block_count"] == block_select]
+        unique_values, indices = np.unique(block_data['Trial_number'], return_index = True)
+        shorter_data = block_data.iloc[indices, :]
+        i = 0
+        for frames_of_interest in [frames_before_stimulus, frames_during_stimulus, frames_after_stimulus]: 
             
-        """The actual classfication"""
-        from sklearn import svm
-        classifier = svm.SVC(kernel = 'linear', C = 1)
-        cv = StratifiedKFold(n_splits=5, random_state=1, shuffle=True)
+            relevant_frames_data = block_data[block_data['Frame_count'].isin(frames_of_interest)]
             
-        """work with k-fold cross-validation"""
-        print("\nNow doing k-fold cross-validation")
-        from sklearn.model_selection import cross_val_score
-        cross_scores = cross_val_score(classifier, x, y, cv=cv, scoring="accuracy", n_jobs = -1)
-        
-        display_scores(cross_scores)
-        mean, std = end_scores(cross_scores)
-        
-        store_all_means[pp, block_select] = mean
-        store_all_std[pp, block_select] = std
+            meanAU_data = np.array([np.mean(relevant_frames_data[relevant_frames_data['Trial_number'] == trial][AU_col], 0) for trial in unique_values])
+            
+            
+            shorter_data.loc[:, AU_col] = meanAU_data
+            
+            """Do the classification"""
+            x, y = shorter_data[AU_col], shorter_data['Cond_binary']
+            #transform the y-data to integers, as this is often required by ML algorithms
+            y = y.astype(np.uint8)
+                
+            """The actual classfication"""
+            from sklearn import svm
+            classifier = svm.SVC(kernel = 'linear', C = 1)
+            cv = StratifiedKFold(n_splits=5, random_state=1, shuffle=True)
+                
+            """work with k-fold cross-validation"""
+            print("\nNow doing k-fold cross-validation")
+            cross_scores = cross_val_score(classifier, x, y, cv=cv, scoring="accuracy", n_jobs = -1)
+            
+            display_scores(cross_scores)
+            mean, std = end_scores(cross_scores)
+            
+            store_all_means[pp, block_select, i] = mean
+            store_all_std[pp, block_select, i] = std
+            
+            i += 1
         #Plot the classification outcome
-        plt.errorbar(20, mean, yerr = std, fmt = formats[block_select], color = colors[block_select], label = 'block {}'.format(block_select))
+        # plt.errorbar(20, mean, yerr = std, fmt = formats[block_select], color = colors[block_select], label = 'block {}'.format(block_select))
         
     # axs.plot(frames, np.repeat(0.5, len(frames)), color = 'red', label = 'Chance level')
     # axs.plot([15,15],[0,5], lw = 2, linestyle ="dashed", color ='y', label ='appeared')
@@ -125,35 +127,37 @@ for pp in participants:
     # fig.legend(handles, labels, loc="upper right")
     # fig.suptitle('Classification for pp {}'.format(pp+1))
     # fig.savefig('FperF_allAUs_pp{}.png'.format(pp+1))
+#%%
 
+formats = ['--o', '--o', '--o']
+colors = ['red', 'green', 'orange']
 
 """Figure to show the average classification over participants"""
-fig, axs = plt.subplots(1, 1)
-axs.set_ylim(0, 1.05)
-significant_frames = np.array([])
-p_values = np.empty((3))
+fig, axs = plt.subplots(1, 3, sharex = True, sharey = True)
+axs[0].set_ylim(0, 1.05)
+p_values = np.empty((3, 3))
+labels = ['F 0-15', 'F 15-45', 'F 45-60']
+plt.xticks(np.array([0, 1, 2]), labels)
 for block in blocks: 
-    means = np.mean(store_all_means[:, block], axis = 0)
-    stds = np.std(store_all_means[:, block], axis = 0)
-    plt.errorbar(20, means, yerr = stds, fmt = formats[block], color = colors[block], label = 'block {}'.format(block))
+    ax = axs[block]
+    ax.set_title("Block  {}".format(block))
+    for frame_subset in range(3): 
+        means = np.nanmean(store_all_means[:, block, frame_subset], axis = 0)
+        stds = np.nanstd(store_all_means[:, block, frame_subset], axis = 0)
+        ax.errorbar(frame_subset, means, yerr = stds, fmt = formats[block], color = colors[block], label = '')
     
-    statistic, p_value = wilcoxon(store_all_means[:, block] - 0.50)
-    p_values[block] = p_value
-    if p_value <= 0.05: 
-        axs.plot(20, 0.3-block*0.05, 'o', color = colors[block])
-        significant_frames = np.append(significant_frames, frame)
-axs.plot(frames, np.repeat(0.5, len(frames)), color = 'black', label = 'Chance level')
-axs.plot([15,15],[0,5], lw = 0.5, linestyle ="dashed", color ='black', label ='appeared')
-axs.plot([46,46],[0,5], lw = 0.5, linestyle ="dashed", color ='black', label ='disappeared')
-handles, labels = axs.get_legend_handles_labels()
-handles = np.delete(handles, [1, 2])
-labels = np.delete(labels, [1, 2])
-fig.legend(handles, labels, loc="upper right")
+        statistic, p_value = wilcoxon(store_all_means[:, block, frame_subset] - 0.50)
+        p_values[block, frame_subset] = p_value
+        if p_value <= 0.05: ax.plot(frame_subset, 1, '*', color = 'grey', linewidth = 0.1)
+    
 fig.suptitle('Classification scores averaged over all pp')
+fig.tight_layout()
 # fig.savefig('F_per_F_averageaccuracyoverallpp.png')
 print(p_values)
 # [0.85895492 0.01367188 0.00195312]
 
+
+# ==> Idea: boxplots instead of mean & std plotting? 
 
 
 
