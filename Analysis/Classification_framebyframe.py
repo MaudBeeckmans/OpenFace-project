c43d4badc0710518c84fe5b5068357305320044c
MaudBeeckmans <maud.beeckmans@outlook.com>
2022-05-06 13:09:50 +0200

    Update Classification_framebyframe.py
    * cleaned
       - some functions to functions file
       - delete pp. 34 & delete block 2 pp 28 and 30 since acc. there < 85%
    * changed the plots a bit
    * option for different correction methods for p-value

--- Analysis/Classification_framebyframe.py
+++ Analysis/Classification_framebyframe.py
@@ -14,37 +14,35 @@ from matplotlib import pyplot as plt
 import pandas as pd
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import StratifiedKFold
-from Functions import import_data, delete_unsuccessful, delete_incorrect_last2blocks
+from sklearn.preprocessing import OrdinalEncoder
+from sklearn.preprocessing import StandardScaler
+from scipy.stats import wilcoxon
+from statsmodels.stats import multitest
+from Functions import import_data, delete_unsuccessful, delete_incorrect_last2blocks, delete_participant
+from Functions import delete_pp_block, display_scores, end_scores
 
-openface_map = r"C:\Users\maudb\Documents\Psychologie\2e master psychologie\Master thesis\Pilot_Master_thesis\OpenFace output"
-all_data = import_data(datafile_path = openface_map)
+delete_below85 = True
+
+openface_map = r"C:\Users\maudb\Documents\Psychologie\2e_master_psychologie\Master_thesis\Pilot_Master_thesis\OpenFace_output"
+all_data = import_data(pp_numbers = np.array([["1", "10"],["11", "20"], ["21", "34"]]), datafile_path = openface_map)
 accurate_data = delete_incorrect_last2blocks(data = all_data)
 Successful_data = delete_unsuccessful(data = accurate_data)
 # Successful_data = all_data[all_data["success"] == 1]
 
+Successful_data["pp_number"] = np.where(Successful_data["pp_number"] == 317, 17, 
+                                        Successful_data["pp_number"])
 
-"""Create some functions to use in the loop"""
-def display_scores(scores):
-    print("Scores:", scores)
-    print("Mean:", scores.mean())
-    print("Standard deviation:", scores.std())
-        
-def end_scores(scores): 
-    mean = scores.mean()
-    std = scores.std()
-    return mean, std 
+Successful_data2 = delete_participant(Successful_data, pp_to_delete = 34)
 
-from sklearn.preprocessing import OrdinalEncoder
-ordinal_encoder = OrdinalEncoder()
+if delete_below85 == True: 
+    # delete pp. 28, block 2 and pp. 30 block 2 (accuracy < 85%)
+    Successful_data2 = delete_pp_block(Successful_data2, 28, 1) # block number as stored (2nd block thus deleted)
+    Successful_data2 = delete_pp_block(Successful_data2, 30, 1)
 
-from sklearn.preprocessing import StandardScaler
-scaler = StandardScaler()
-from scipy.stats import wilcoxon
-from statsmodels.stats import multitest
 
 #%%
 """Select which subset of the data you'd like to use"""
-participants = np.arange(0, 10, 1)
+participants = np.unique(Successful_data2['pp_number']).astype(int)
 blocks = np.array([0, 1, 2])
 formats = ['--o', '--x', '--v']
 colors = ['red', 'green', 'orange']
@@ -53,13 +51,15 @@ frames = np.arange(0, 60, 1)
 store_all_means = np.empty([participants.shape[0], blocks.shape[0], frames.shape[0]])
 store_all_std = np.empty([participants.shape[0], blocks.shape[0], frames.shape[0]])
 
+scaler = StandardScaler()
+ordinal_encoder = OrdinalEncoder()
+
 for pp in participants: 
     print("we're at pp {}".format(pp))
-    fig, axs = plt.subplots(1, 1)
-    axs.set_ylim(0, 1.05)
+    # fig, axs = plt.subplots(1, 1)
+    # axs.set_ylim(0, 1.05)
     
-    # sub_data_df = all_data.loc[all_data["pp_number"] == pp+1]
-    sub_data_df = Successful_data.loc[Successful_data["pp_number"] == pp+1]
+    sub_data_df = Successful_data.loc[Successful_data["pp_number"] == pp]
     for block_select in blocks: 
     
         data_df = sub_data_df.loc[sub_data_df["block_count"] == block_select] # 60 frames per trial, 150 trials per block: 9000 rows
@@ -79,24 +79,12 @@ for pp in participants:
             data_f = data_df.loc[data_df["frame"] ==this_frame]
             data_f = data_f.reset_index()
             
-            # split = StratifiedShuffleSplit(n_splits = 1, test_size = 0.2, random_state = 42)
-            # for train_index, test_index in split.split(data_f, data_f["Affect"]):
-            #     strat_train_set = data_f.loc[train_index]
-            #     strat_test_set = data_f.loc[test_index]
-            """Just as a check whether it worked correctly: 
-                - train_set: 80 pos, 80 neg
-                - test_set: 20 pos, 20 neg"""
-            
-            #strat_test_set["Affect"].value_counts() / len(strat_test_set)
-            #strat_train_set["Affect"].value_counts() / len(strat_train_set)
             
             """create the x and y variables for strat_train_set & strat_test_set"""
             AU_col = [col for col in data_f.columns if ('AU' in col and '_r' in col)] 
             x, y = data_f[AU_col], data_f['Cond_binary']
-            #test_x, test_y = strat_test_set[AU_col], strat_test_set['Cond_binary']
             #transform the y-data to integers, as this is often required by ML algorithms
             y = y.astype(np.uint8)
-            #test_y = test_y.astype(np.uint8)
             
             """Feature-scaling: might have to do this (see book page 109)
             - important: fit the scalers to training set only
@@ -113,15 +101,12 @@ for pp in participants:
             """The actual classfication"""
             from sklearn import svm
             classifier = svm.SVC(kernel = 'linear', C = 1)
-            # classifier.fit(train_x, train_y)
-            # print("The score when trained on train-set, tested on test-set")
-            # print(classifier.score(test_x, test_y)
             
             
             """work with k-fold cross-validation"""
             # print("\nNow doing k-fold cross-validation")
             # cv = StratifiedKFold(n_splits=10, random_state=1, shuffle=True)
-            cv = StratifiedKFold(n_splits=5, shuffle=True)
+            cv = StratifiedKFold(n_splits=5, shuffle=True, random_state = 1473)
             
             # cross_scores = cross_val_score(classifier, train_x_standardized, train_y, cv=10, scoring="accuracy")
             cross_scores = cross_val_score(classifier, x, y, cv=cv, scoring="accuracy", n_jobs = -1)
@@ -132,45 +117,43 @@ for pp in participants:
             
             all_mean.append(mean)
             all_std.append(std)
-        store_all_means[pp, block_select, :] = all_mean
-        store_all_std[pp, block_select, :] = all_std
+        store_all_means[pp-1, block_select, :] = all_mean
+        store_all_std[pp-1, block_select, :] = all_std
         #Plot the classification outcome
-        plt.errorbar(frames, all_mean, yerr = all_std, fmt = formats[block_select], color = colors[block_select], label = 'block {}'.format(block_select))
+        # plt.errorbar(frames, all_mean, yerr = all_std, fmt = formats[block_select], color = colors[block_select], label = 'block {}'.format(block_select))
         
-    axs.plot(frames, np.repeat(0.5, len(frames)), color = 'red', label = 'Chance level')
-    axs.plot([15,15],[0,5], lw = 2, linestyle ="dashed", color ='y', label ='appeared')
-    axs.plot([46,46],[0,5], lw = 2, linestyle ="dashed", color ='y', label ='disappeared')
-    handles, labels = axs.get_legend_handles_labels()
-    fig.legend(handles, labels, loc="upper right")
-    fig.suptitle('Classification for pp {}'.format(pp+1))
+    # axs.plot(frames, np.repeat(0.5, len(frames)), color = 'red', label = 'Chance level')
+    # axs.plot([15,15],[0,5], lw = 2, linestyle ="dashed", color ='y', label ='appeared')
+    # axs.plot([46,46],[0,5], lw = 2, linestyle ="dashed", color ='y', label ='disappeared')
+    # handles, labels = axs.get_legend_handles_labels()
+    # fig.legend(handles, labels, loc="upper right")
+    # fig.suptitle('Classification for pp {}'.format(pp))
     # fig.savefig('FperF_allAUs_pp{}.png'.format(pp+1))
 #%%
 """Figure to show the average classification over participants"""
 
-frames_of_interest = np.arange(15, 60, 1)
+correction = 'holm' # should be holm or fdr 
+
+frames_of_interest = np.arange(0, 60, 1)
 
 fig, axs = plt.subplots(1, 1)
-axs.set_ylim(0, 1.05)
+axs.set_ylim(0.35, 1.05)
+# axs.set_ylim(0, 0.7)
 significant_frames = np.array([])
 p_values = np.empty((3, 60))
+blocks = np.array([0, 1, 2])
+# blocks = [0, 1]
 for block in blocks: 
     means = np.mean(store_all_means[:, block, :], axis = 0)
     stds = np.std(store_all_means[:, block, :], axis = 0)
-    plt.errorbar(frames, means, yerr = stds, fmt = formats[block], color = colors[block], label = 'block {}'.format(block))
+    plt.errorbar(frames, means, yerr = stds, fmt = formats[block], color = colors[block], label = 'block {}'.format(block+1))
     for frame in frames: 
         statistic, p_value = wilcoxon(store_all_means[:, block, frame] - 0.50, alternative = 'greater')
         p_values[block, frame] = p_value
-        # if p_value <= 0.05: 
-        #     axs.plot(frame, 0.4-block*0.05, 'o', color = colors[block], markersize = 2)
-        #     significant_frames = np.append(significant_frames, frame)
 
-    signif_frames, corrected_pvals = multitest.fdrcorrection(p_values[block, frames_of_interest], alpha=0.05, method='indep', is_sorted=False)
-    if np.any(signif_frames == True): axs.plot(frames_of_interest[signif_frames], np.repeat(0.35-block*0.05, frames_of_interest[signif_frames].shape[0]), 'o', color = colors[block], markersize = 5)
-# """Below: code if corrections are to happen for p_values over all blocks simultaneously"""
-# signif_frames, corrected_pvals = multitest.fdrcorrection(p_values[:, frames_of_interest].reshape(blocks.shape[0]*frames_of_interest.shape[0]), alpha=0.05, method='indep', is_sorted=False)    
-# signif_frames = signif_frames.reshape((blocks.shape[0], frames_of_interest.shape[0]))
-# for block in blocks: 
-#     if np.any(signif_frames[block, :] == True): axs.plot(frames_of_interest[signif_frames[block, :]], np.repeat(0.2-block*0.05, frames_of_interest[signif_frames[block, :]].shape[0]), 'o', color = colors[block], markersize = 5)
+    if correction == 'fdr': signif_frames, corrected_pvals = multitest.fdrcorrection(p_values[block, frames_of_interest], alpha=0.05, method='indep', is_sorted=False)
+    elif correction == 'holm': signif_frames, corrected_pvals, b, c = multitest.multipletests(p_values[block, frames_of_interest], alpha = 0.05, method = 'holm')
+    if np.any(signif_frames == True): axs.plot(frames_of_interest[signif_frames], np.repeat(0.45-block*0.025, frames_of_interest[signif_frames].shape[0]), 'o', color = colors[block], markersize = 5)
     
        
 axs.plot(frames, np.repeat(0.5, len(frames)), color = 'black', label = 'Chance level')
@@ -179,9 +162,14 @@ axs.plot([46,46],[0,5], lw = 0.5, linestyle ="dashed", color ='black', label ='d
 handles, labels = axs.get_legend_handles_labels()
 handles = np.delete(handles, [1, 2])
 labels = np.delete(labels, [1, 2])
-fig.legend(handles, labels, loc="upper right")
+fig.legend(handles, labels, loc="center right")
 fig.suptitle('Classification scores averaged over all pp')
-# fig.savefig('F_per_F_averageaccuracyoverallpp.png')
+axs.set_title("Correction method: {}, frames included: {}-{}".format(correction, 
+                                                                     frames_of_interest[0]+1, 
+                                                                     frames_of_interest[-1]+1))
+fig.savefig(os.path.join(os.getcwd(), 'Classification_plots', 'F_per_F_{}_frames{}to{}_below85deleted{}.png'.format(correction, 
+                                                                          frames_of_interest[0]+1, 
+                                                                          frames_of_interest[-1]+1, delete_below85)))
 
 #%%
 
@@ -196,14 +184,6 @@ for train_ix, test_ix in cv.split(x, y):
 	test_0, test_1 = len(test_y[test_y==0]), len(test_y[test_y==1])
 	print('>Train: 0=%d, 1=%d, Test: 0=%d, 1=%d' % (train_0, train_1, test_0, test_1))
 
-
-
-#%%
-
-"""Evaluating the p-values"""
-for block in blocks: 
-    for frame in frames: 
-        statistic, p_value = wilcoxon(store_all_means[:, block, frame] - 0.50)
         
         
 
