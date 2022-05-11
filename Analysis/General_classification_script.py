423f3b081039dd521bcbd66a895cecc658817ed2
MaudBeeckmans <maud.beeckmans@outlook.com>
2022-05-11 09:08:45 +0200

    Update General_classification_script.py
    Script used to run the 1000 reps classifications for meanAU and FperF!

--- Analysis/General_classification_script.py
+++ Analysis/General_classification_script.py
@@ -57,8 +57,6 @@ Conditions_data_encoded = ordinal_encoder.fit_transform(Conditions_data)
 cleaned_data.insert(2, "Cond_binary", Conditions_data_encoded, True)
 
 #%%
-analysis = 'meanAU' # analysis_type should be 'FperF' or 'meanAU'
-
 def select_frames(analysis_type = 'FperF', data = cleaned_data): 
     if analysis_type == 'FperF': 
         frame_selection = np.unique(data.Frame_count).astype(int)
@@ -89,58 +87,56 @@ def take_mean(start_data = None, analysis_type = 'FperF'):
         final_data = final_data.dropna()
     return final_data
 #%%
+analysis = 'FperF' # analysis_type should be 'FperF' or 'meanAU'
 k_folds = 5
-n_reps = 1
+n_reps = 1000
 
 # frame selection: array when FperF; list when meanAU    
 frame_selection, frameselection_names, n_subsets = select_frames(analysis_type = analysis)
 participants = np.unique(cleaned_data.pp_number).astype(int)
 blocks = np.unique(cleaned_data.block_count).astype(int)
-seed_values = np.random.randint(0, 100000, n_reps)
 
 #Create empty arrays to store all the obtained mean accuracies for each pp. in each block at each frame_selection and within each repetition
-store_all_means = np.empty([n_reps, participants.shape[0], blocks.shape[0], n_subsets])
-store_all_std = np.empty([n_reps, participants.shape[0], blocks.shape[0], n_subsets])
+store_all_means = np.empty([participants.shape[0], blocks.shape[0], n_subsets])
+store_all_std = np.empty([participants.shape[0], blocks.shape[0], n_subsets])
 
-for rep, seed in zip(range(n_reps), seed_values):
-    print("we're at repetition {}".format(rep))
-    #ipp: starts from 0; pp starts from 1
-    for ipp, pp in zip(participants-1, participants): 
-        print("we're at pp {}".format(pp))
-        #select the data relevant for this participant 
-        pp_data = cleaned_data.loc[cleaned_data["pp_number"] == pp]
-        #iblock: starts from 0, block: starts from 1
-        for iblock, block in zip(blocks, blocks+1): 
-            # for pp. 28 and 30 in block 2 data not used 
-            if (pp == 28 and block == 2) or (pp == 30 and block == 2): 
-                store_all_means[rep, ipp, iblock, :] = np.nan
-                store_all_std[rep, ipp, iblock, :] = np.nan
-            else: 
-                block_data = pp_data[pp_data.block_count == iblock]
-                final_data = take_mean(start_data = block_data, analysis_type = analysis)
+
+for ipp, pp in zip(participants-1, participants): 
+    print("we're at pp {}".format(pp))
+    #select the data relevant for this participant 
+    pp_data = cleaned_data.loc[cleaned_data["pp_number"] == pp]
+    #iblock: starts from 0, block: starts from 1
+    for iblock, block in zip(blocks, blocks+1): 
+        # for pp. 28 and 30 in block 2 data not used 
+        if (pp == 28 and block == 2) or (pp == 30 and block == 2): 
+            store_all_means[ipp, iblock, :] = np.nan
+            store_all_std[ipp, iblock, :] = np.nan
+        else: 
+            block_data = pp_data[pp_data.block_count == iblock]
+            final_data = take_mean(start_data = block_data, analysis_type = analysis)
+            
+            all_mean = []
+            all_std = []
+            for selected_frames in frameselection_names: 
+                subset_data = final_data.loc[final_data.Frame_count == selected_frames]
+                subset_data = subset_data.reset_index()
                 
-                all_mean = []
-                all_std = []
-                for selected_frames in frameselection_names: 
-                    subset_data = final_data.loc[final_data.Frame_count == selected_frames]
-                    subset_data = subset_data.reset_index()
-                    
-                    x, y = subset_data[AU_cols], subset_data['Cond_binary']
-                    #transform the y-data to integers, as this is often required by ML algorithms
-                    y = y.astype(np.uint8)
-                    
-                    #Create the actual classifier (non-trained)
-                    classifier = svm.SVC(kernel = 'linear', C = 1)
-                    cv = StratifiedKFold(n_splits=k_folds, shuffle=True, random_state = seed)
-                    # cv = RepeatedStratifiedKFold(n_splits = k_folds, n_repeats = 2)
-                    cross_scores = cross_val_score(classifier, x, y, cv=cv, scoring="accuracy", n_jobs = -1)
-                    # cross_scores = cross_val_score(classifier, x, y, cv=cv, scoring="roc_auc", n_jobs = -1)
-                    mean, std = end_scores(cross_scores)
-                    
-                    all_mean.append(mean)
-                    all_std.append(std)
-                store_all_means[rep, ipp, iblock, :] = all_mean
-                store_all_std[rep, ipp, iblock, :] = all_std
+                x, y = subset_data[AU_cols], subset_data['Cond_binary']
+                #transform the y-data to integers, as this is often required by ML algorithms
+                y = y.astype(np.uint8)
+                
+                #Create the actual classifier (non-trained)
+                classifier = svm.SVC(kernel = 'linear', C = 1)
+                # cv = StratifiedKFold(n_splits=k_folds, shuffle=True, random_state = seed)
+                cv = RepeatedStratifiedKFold(n_splits = k_folds, n_repeats = n_reps)
+                cross_scores = cross_val_score(classifier, x, y, cv=cv, scoring="accuracy", n_jobs = -1)
+                # cross_scores = cross_val_score(classifier, x, y, cv=cv, scoring="roc_auc", n_jobs = -1)
+                mean, std = end_scores(cross_scores)
+                if np.any(cross_scores == np.nan): print("nan value observed")
+                all_mean.append(mean)
+                all_std.append(std)
+            store_all_means[ipp, iblock, :] = all_mean
+            store_all_std[ipp, iblock, :] = all_std
 
 #Store the results in a file!
 array_dir = os.path.join(os.getcwd(), 'Stored_results')
@@ -156,7 +152,7 @@ blocks = np.array([0, 1, 2])
 
 formats = ['--o', '--o', '--o']
 colors = ['red', 'green', 'orange']
-averaged_means = np.nanmean(store_all_means, axis = 0)
+averaged_means = store_all_means
 
 if analysis == 'meanAU': 
     fig, axs = plt.subplots(1, n_subsets, sharex = True, sharey = True)
@@ -171,11 +167,10 @@ if analysis == 'meanAU':
             means = np.nanmean(averaged_means[:, iblock, isubset], axis = 0)
             stds = np.nanstd(averaged_means[:, iblock, isubset], axis = 0)
             # ax.errorbar(isubset, means, yerr = stds, fmt = formats[iblock], color = colors[iblock], label = '')
-            x = averaged_means[:, iblock, isubset] 
-            x = x[~np.isnan(x)]
-            ax.boxplot(x, positions = [isubset])
-            statistic, p_value = wilcoxon(x - 0.50, alternative = 'greater')
-            """Problem: p-values do take nan into account I think!"""
+            z = averaged_means[:, iblock, isubset] 
+            z = z[~np.isnan(z)]
+            ax.boxplot(z, positions = [isubset], showmeans = True)
+            statistic, p_value = wilcoxon(z - 0.50, alternative = 'greater')
             p_values[iblock, isubset] = p_value
         if correction == 'fdr': signif_frames, corrected_pvals = multitest.fdrcorrection(p_values[iblock, :], alpha=0.05, method='indep', is_sorted=False)
         elif correction == 'holm': signif_frames, corrected_pvals, b, c = multitest.multipletests(p_values[iblock, :], alpha = 0.05, method = 'holm')
@@ -197,16 +192,15 @@ elif analysis == 'FperF':
     if blocks.shape[0] != 3:  axs.set_ylim(0.35, 0.6)
     significant_frames = np.array([])
     p_values = np.empty((blocks.shape[0], n_subsets))
-    averaged_means = np.nanmean(store_all_means, axis = 0)
 
     for iblock, block in zip(blocks, blocks+1): 
         means = np.nanmean(averaged_means[:, iblock, :], axis = 0)
         stds = np.nanstd(averaged_means[:, iblock, :], axis = 0)
         plt.errorbar(frame_selection, means, yerr = stds, fmt = formats[iblock], color = colors[iblock], label = 'block {}'.format(block))
         for frame in frame_selection: 
-            x = averaged_means[:, iblock, frame] 
-            x = x[~np.isnan(x)]
-            statistic, p_value = wilcoxon(x - 0.50, alternative = 'greater')
+            z = averaged_means[:, iblock, frame] 
+            z = z[~np.isnan(z)]
+            statistic, p_value = wilcoxon(z - 0.50, alternative = 'greater')
             p_values[iblock, frame] = p_value
 
         if correction == 'fdr': signif_frames, corrected_pvals = multitest.fdrcorrection(p_values[iblock, frames_corrected_for], alpha=0.05, method='indep', is_sorted=False)
@@ -215,8 +209,8 @@ elif analysis == 'FperF':
         
            
     axs.plot(frame_selection, np.repeat(0.5, n_subsets), color = 'black', label = 'Chance level')
-    axs.plot([15,15],[0,5], lw = 0.5, linestyle ="dashed", color ='black', label ='appeared')
-    axs.plot([46,46],[0,5], lw = 0.5, linestyle ="dashed", color ='black', label ='disappeared')
+    axs.plot([14,14],[0,5], lw = 0.5, linestyle ="dashed", color ='black', label ='appeared')
+    axs.plot([45,45],[0,5], lw = 0.5, linestyle ="dashed", color ='black', label ='disappeared')
     handles, labels = axs.get_legend_handles_labels()
     handles = np.delete(handles, [1, 2])
     labels = np.delete(labels, [1, 2])
@@ -225,9 +219,10 @@ elif analysis == 'FperF':
     axs.set_title("Correction method: {}, frames included: {}-{}".format(correction, 
                                                                          frames_corrected_for[0]+1, 
                                                                          frames_corrected_for[-1]+1))
-    fig.savefig(os.path.join(os.getcwd(), 'Classification_plots', 'F_per_F_{}_frames{}to{}_below85deleted{}.png'.format(correction, 
+    fig.savefig(os.path.join(os.getcwd(), 'Classification_plots', 'F_per_F_{}_frames{}to{}_below85deleted{}_{}folds_{}reps.png'.format(correction, 
                                                                               frames_corrected_for[0]+1, 
-                                                                              frames_corrected_for[-1]+1, delete_below85)))
+                                                                              frames_corrected_for[-1]+1, delete_below85, 
+                                                                              k_folds, n_reps)))
 
 
 
@@ -236,6 +231,15 @@ elif analysis == 'FperF':
 
 
 
+#%%
+for train_ix, test_ix in cv.split(x, y):
+	# select rows
+	train_X, test_X = x.iloc[train_ix, :], x.iloc[test_ix, :]
+	train_y, test_y = y[train_ix], y[test_ix]
+	# summarize train and test composition
+	train_0, train_1 = len(train_y[train_y==0]), len(train_y[train_y==1])
+	test_0, test_1 = len(test_y[test_y==0]), len(test_y[test_y==1])
+	print('>Train: 0=%d, 1=%d, Test: 0=%d, 1=%d' % (train_0, train_1, test_0, test_1))
 
 
 
